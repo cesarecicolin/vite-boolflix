8f5f6ecd0d856e7c70e97f6a889f54378174a3c7
cesarecicolin <cece.caster@gmail.com>
2023-11-29 14:32:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from "axios"
+import {store} from "./store.js"
+export default {
+  data(){
+    return{
+      store
+    }
+  }
+}
+</script>
+
+<template>
+ <h1>ciao bro <i class="fa-solid fa-magnifying-glass"></i></h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+</style>
